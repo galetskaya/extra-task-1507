f5de8913137196a614adf3fff1aa6c5907afcb95
galetskaya <li-gn@yandex.ru>
2022-07-15 23:30:48 +0200

    Fibonacci numbers task

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//На входе функции натуральное число N, и потом формируется массив из N членов ряда Фибоначчи
+
+Console.WriteLine("Eneter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string masFibonn(int arg)
+{
+    int[] massive = new int[arg];
+    int y = 0;
+
+    for(int x = 0; x < massive.Length; x++) 
+    {
+        if(x < 2)
+        {
+            massive[x] = y;
+            y++;
+        }
+
+        else
+        {
+            massive[x] = massive[x-1] + massive[x-2];
+        }
+    }
+    string mass2 = string.Join(',',massive);
+    return mass2;
+}
+
+Console.WriteLine(masFibonn(num));
