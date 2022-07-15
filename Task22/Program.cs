ef007299556f954771e5930207076b76b72f5588
galetskaya <li-gn@yandex.ru>
2022-07-15 23:31:15 +0200

    Squered from N task

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+//квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+Console.WriteLine("Eneter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void findQuar(int arg)
+{
+    int y = 1;
+    int a = 0;
+
+    while(y <= arg)
+    {
+        a = y * y;
+        y++;
+        Console.Write(a);
+        Console.Write(", ");
+    }
+}
+
+findQuar(num);
